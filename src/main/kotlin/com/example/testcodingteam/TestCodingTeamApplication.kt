2de201332116eddcab09c1e295c3a139b93d19c6
danila.veselkov <Danila.Veselkov@x5.ru>
2022-08-11 16:54:06 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/testcodingteam/TestCodingTeamApplication.kt
@@ -0,0 +1,11 @@
+package com.example.testcodingteam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestCodingTeamApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestCodingTeamApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/testcodingteam/TestCodingTeamApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.testcodingteam
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestCodingTeamApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
